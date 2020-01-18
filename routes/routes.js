95e3d1f3091db5b25c4a2ca84a1cc3ca0f8741bc
Unknown <tarteserviceinformatique@sfr.fr>
2020-01-18 12:35:42 +0100

    add routes get and post for actors and directors

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,55 @@
-var appRouter = function (app) {
+var appRouter = (app) => {
     app.get("/", function(req, res) {
-      res.status(200).send("Welcome to our restful API");
+      res.status(200).send("Route racine en get");
+    });
+    app.get("/realisateur", (req, res) => {
+      // send the list of all directors
+      res.status(200).send("Route realisateur en get");
+    });
+    app.get("/realisateur/:nomRea", (req, res) => {
+      // send all DVDs of a director
+      const nomRealisateur = req.params.nomRea;
+   
+      if (nomRealisateur!=="") {
+        res.status(200).send(`route realisateur pour ${nomRealisateur}`);
+      } else {
+        res.status(400).send({ message: 'route realisateur mais avec un nom vide' });
+      }
+    });
+    app.get("/acteur", (req, res) => {
+      // send all actors
+      res.status(200).send("Route acteur en get");
+    });
+    app.get("/acteur/:nomAct", (req, res) => {
+      // send all films of an actor
+      const nomActeur = req.params.nomAct;
+   
+      if (nomActeur!=="") {
+        res.status(200).send(`route acteur pour ${nomActeur}`);
+      } else {
+        res.status(400).send({ message: 'route acteur mais avec un nom vide' });
+      }
+    });
+    
+    app.post("/realisateur/:nomRea", (req, res) => {
+      // add a director
+      const nomRealisateur = req.params.nomRea;
+   
+      if (nomRealisateur!=="") {
+        res.status(200).send(`route ajout realisateur pour ${nomRealisateur}`);
+      } else {
+        res.status(400).send({ message: 'route ajout realisateur mais avec un nom vide' });
+      }
+    });
+    app.post("/acteur/:nomAct", (req, res) => {
+      // add an actor
+      const nomActeur = req.params.nomAct;
+   
+      if (nomActeur!=="") {
+        res.status(200).send(`route ajout acteur pour ${nomActeur}`);
+      } else {
+        res.status(400).send({ message: 'route ajout acteur mais avec un nom vide' });
+      }
     });
   }
   
